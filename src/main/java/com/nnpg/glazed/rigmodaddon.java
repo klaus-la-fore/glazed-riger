08875bc6fc338180387bd697f50ebfc306ea3baf
klaus-la-fore <aixo90982248@gmail.com>
2026-05-01 01:46:00 +0400

    Add GamblingDropperRig module for forced RNG seed

---
+++ src/main/java/com/nnpg/glazed/rigmodaddon.java
@@ -0,0 +1,25 @@
+package com.nnpg.glazed.modules.main;
+
+import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
+import meteordevelopment.meteorclient.settings.IntSetting;
+import com.nnpg.glazed.GlazedAddon;
+
+public class GamblingDropperRig extends Module {
+    private final SettingGroup sgGeneral = settings.getDefaultGroup();
+    
+    private final Setting<Integer> forcedSeed = sgGeneral.add(new IntSetting.Builder()
+        .name(\"forced-seed\")
+        .description(\"The seed to force for the dropper.\")
+        .defaultValue(0xDEADBEEF)
+        .build());
+
+    public GamblingDropperRig() {
+        super(GlazedAddon.CATEGORY, \"gambling-dropper-rig\", \"Forces RNG seed for DonutSMP droppers.\");
+    }
+
+    public int getRiggedSeed() {
+        return forcedSeed.get();
+    }
+}
