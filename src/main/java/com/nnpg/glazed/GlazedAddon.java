eeac345038f9778ee8a3b8f4328ec99828308f3c
klaus-la-fore <aixo90982248@gmail.com>
2026-05-01 01:58:54 +0400

    Update GlazedAddon.java

--- src/main/java/com/nnpg/glazed/GlazedAddon.java
+++ src/main/java/com/nnpg/glazed/GlazedAddon.java
@@ -1,8 +1,7 @@
-package com.nnpg.glazed.modules.main;
+package com.nnpg.glazed; // Corrected package
 
-import com.nnpg.glazed.modules.main.GamblingDropperRig;
 import com.nnpg.glazed.modules.esp.*;
-import com.nnpg.glazed.modules.main.*;
+import com.nnpg.glazed.modules.main.*; // This includes GamblingDropperRig automatically
 import com.nnpg.glazed.modules.pvp.*;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
 import meteordevelopment.meteorclient.systems.modules.Modules;
@@ -23,86 +22,9 @@ public class GlazedAddon extends MeteorAddon {
 
     @Override
     public void onInitialize() {
-        Modules.get().add(new SpawnerProtect());
-        Modules.get().add(new AntiTrap());
-        Modules.get().add(new CoordSnapper());
-        Modules.get().add(new PlayerDetection());
-        Modules.get().add(new AHSniper());
-        Modules.get().add(new RTPer());
-        Modules.get().add(new ShulkerDropper());
-        Modules.get().add(new AutoSell());
-        Modules.get().add(new SpawnerDropper());
-        Modules.get().add(new AutoShulkerOrder());
-        Modules.get().add(new AutoOrder());
-        Modules.get().add(new HideScoreboard());
-        Modules.get().add(new CrystalMacro());
-        Modules.get().add(new AHSell());
-        Modules.get().add(new AnchorMacro());
-        Modules.get().add(new OneByOneHoles());
-        Modules.get().add(new KelpESP());
-        Modules.get().add(new DripstoneESP());
-        Modules.get().add(new RotatedDeepslateESP());
-        Modules.get().add(new CrateBuyer());
-        Modules.get().add(new WanderingESP());
-        Modules.get().add(new VillagerESP());
-        Modules.get().add(new AdvancedStashFinder());
-        Modules.get().add(new TpaMacro());
-        Modules.get().add(new TabDetector());
-        Modules.get().add(new OrderSniper());
-        Modules.get().add(new LamaESP());
-        Modules.get().add(new PillagerESP());
-        Modules.get().add(new HoleTunnelStairsESP());
-        Modules.get().add(new CoveredHole());
-        Modules.get().add(new ClusterFinder());
-        Modules.get().add(new AutoShulkerShellOrder());
-        Modules.get().add(new EmergencySeller());
-        Modules.get().add(new RTPEndBaseFinder());
-        Modules.get().add(new ShopBuyer());
-        Modules.get().add(new OrderDropper());
-        Modules.get().add(new CollectibleESP());
-        Modules.get().add(new SpawnerNotifier());
-        Modules.get().add(new VineESP());
-        Modules.get().add(new ChunkFinder());
-        Modules.get().add(new BlockNotifier());
-        Modules.get().add(new SpawnerOrder());
-        Modules.get().add(new RegionMap());
-        Modules.get().add(new NoBlockInteract());
-        Modules.get().add(new BeehiveESP());
-        Modules.get().add(new WindPearlMacro());
-        Modules.get().add(new SwordPlaceObsidian());
-        Modules.get().add(new ChestAndShulkerStealer());
-        Modules.get().add(new DoubleAnchorMacro());
-        Modules.get().add(new AutoDoubleHand());
-        Modules.get().add(new SweetBerryESP());
-        Modules.get().add(new PistonESP());
-        Modules.get().add(new TpaAllMacro());
-        Modules.get().add(new RTPNetherBaseFinder());
-        Modules.get().add(new HomeReset());
-        Modules.get().add(new KeyPearl());
-        Modules.get().add(new DrownedTridentESP());
-        Modules.get().add(new RTPBaseFinder());
-        Modules.get().add(new HoverTotem());
-        Modules.get().add(new TunnelBaseFinder());
-        Modules.get().add(new AimAssist());
-        Modules.get().add(new SkeletonESP());
-        Modules.get().add(new RainNoti());
-        Modules.get().add(new AutoPearlChain());
-        Modules.get().add(new AutoBlazeRodOrder());
-        Modules.get().add(new BlazeRodDropper());
-        Modules.get().add(new BreachSwap());
-        Modules.get().add(new FakeScoreboard());
-        Modules.get().add(new AutoInvTotem());
-        Modules.get().add(new FreecamMining());
-        Modules.get().add(new BedrockVoidESP());
-        Modules.get().add(new UIHelper());
-        Modules.get().add(new ShieldBreaker());
-        Modules.get().add(new InvisESP());
-        Modules.get().add(new AutoTotemOrder());
-        Modules.get().add(new LightESP());
-        Modules.get().add(new PremiumTunnelBaseFinder());
-        Modules.get().add(new AdminList());
+        // ... (keep all your existing module registrations)
         Modules.get().add(new AutoTreeFarmer());
-        Modules.get().add(new GamblingDropperRig());
+        Modules.get().add(new GamblingDropperRig()); // Now it will find it
     }
 
     @EventHandler
